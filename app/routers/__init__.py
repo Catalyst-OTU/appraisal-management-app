fa12cd189cc72fe665d3abba1633b4342153348f
moro23 <billhab66@gmail.com>
2023-06-07 11:58:42 +0000

    making routers a pythong subpackage.

---
+++ app/routers/__init__.py
@@ -0,0 +1,9 @@
+from routers.users import * 
+
+from database import Base, engine
+
+__all__ = [
+    'users',
+]
+#tables=[table for table in Base.metadata.sorted_tables if table.schema=='public' or table.schema=='global']
+Base.metadata.create_all(bind=engine) 
