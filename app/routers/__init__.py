d82a33adeee8a65272334ed0b3bdd7cf9c31a7c6
moro23 <billhab66@gmail.com>
2023-06-08 12:16:02 +0000

    updated the routers python subpackage.

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,6 +4,9 @@ from database import Base, engine
 
 __all__ = [
     'users',
+    'staff',
+    'department_type',
+    'user_type'
 ]
 #tables=[table for table in Base.metadata.sorted_tables if table.schema=='public' or table.schema=='global']
 Base.metadata.create_all(bind=engine) 
